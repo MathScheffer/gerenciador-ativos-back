cf20556e482ca084d097f34f9dc488290e97e48f
MathScheffer <matheusmartinsscheffer@hotmail.com>
2025-06-26 22:10:51 -0300

    alterar uri

--- config/conexao.db.js
+++ config/conexao.db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize")
 
 exports.sequelize = new Sequelize(
   //"postgresql://postgres:P4ssw0rdP0STGR3SS@db.ogrfmpvyjnybliwzlqsl.supabase.co:5432/postgres",
-  "postgres://postgres.ogrfmpvyjnybliwzlqsl:P4ssw0rdP0STGR3SS@db@aws-0-sa-east-1.pooler.supabase.com:5432/postgres",
+  "postgresql://postgres.ogrfmpvyjnybliwzlqsl:P4ssw0rdP0STGR3SS@aws-0-sa-east-1.pooler.supabase.com:5432/postgres",
   {
     dialect: "postgres",    
     dialectOptions: {
